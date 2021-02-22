a093fc3c5d18590f51817eeb53ccc9bdafd38d48
Benjamin Lavian <benylavian@gmail.com>
2021-02-22 12:54:59 -0500

    budget_grpup model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .db import db
 from .user import User
 from .month import Month
+from .budget_group import budget_group
+from .budget_item import budget_item
---
+++ app/models/budget_group.py
@@ -0,0 +1,27 @@
+from .db import db
+from flask_login import UserMixin
+
+
+class budget_group(db.Model, UserMixin):
+    __tablename__ = 'budget_groups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), nullable=False)
+    description = db.Column(db.String(50))
+    total_amount = db.Column(db.Float(3, 2), nullable=False, default=0)
+    total_paid = db.Column(db.Float(3, 2), nullable=False, default=0)
+    month_id = db.Column(db.Integer, db.ForeignKey('month.id'), nullable=False)
+    createdAt = db.Column(db.DateTime)
+    updatedAt = db.Column(db.DateTime)
+    months = db.relationship("Month", backref='budget_groups')
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "total_amount": self.total_amount,
+            "total_paid": self.total_paid,
+            "month_id": self.month_id,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt,
+        }
