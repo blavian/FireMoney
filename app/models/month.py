424c282a7a8b7f56cd5f98628391d4559ec8ed2b
Benjamin Lavian <benylavian@gmail.com>
2021-02-21 21:45:19 -0500

    month model

---
+++ app/models/month.py
@@ -0,0 +1,21 @@
+from .db import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+  __tablename__ = 'months'
+
+  id = db.Column(db.Integer, primary_key=True)
+  month_int = db.Column(db.Integer(40), nullable=False)
+  year_int = db.Column(db.Integer(40), nullable=False)
+  total_amount = db.Column(db.Float, nullable=False)
+  total_paid = db.Column(db.Float, nullable=False)
+  user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable = False)
+
+  def to_dict(self):
+    return {
+        "month_int": self.month_int,
+        "year_int": self.year_int,
+        "total_amount": self.total_amount,
+        "total_paid": self.total_paid,
+        "user_id": self.user_id,
+    }
--- app/models/user.py
+++ app/models/user.py
@@ -6,8 +6,8 @@ class User(db.Model, UserMixin):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
+  username = db.Column(db.String(30), nullable = False, unique = True)
+  email = db.Column(db.String(256), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False)
 
 
