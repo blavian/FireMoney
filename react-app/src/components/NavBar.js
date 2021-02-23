23b2f0b67453c8673325bedd35b168ef9f36b07b
jh9374 <39279427+jh9374@users.noreply.github.com>
2021-02-22 18:38:08 -0600

    hamburger menu is setup for guest user at the moment

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,74 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import icon from '../images/Icon.png';
+import hbmenu_icon from "../images/HBMenu.png"
+import closeHB_icon from "../images/CloseHBMenu.png"
+import login_icon from "../images/login.png"
+import signup_icon from "../images/signup.png"
+
 
 const NavBar = ({ setAuthenticated }) => {
+
+  const [showhbmenu, setShowHBMenu] = useState(false);
+
+  const hbtrigger = () => setShowHBMenu(!showhbmenu);
+  
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
+      <div>
+        <img id="firemoney_icon" src={icon} alt="mountain road" />
+        <NavLink class="home_link" to="/" exact={true} activeClassName="active">
+          firemoney
+        </NavLink>
+      </div>
+      <div>
+        <div className="hb_menu_button" >
+          <Link to="#" className="hbmenu_link">
+            <img src={hbmenu_icon} alt="menu" onClick={hbtrigger}/>
+          </Link>
+        </div>
+        { showhbmenu &&
+        (<div className="hb_menu">
+              <NavLink to="/login" exact={true} activeClassName="active">
+                <img src={login_icon} alt="login"/>
+                <span>Login</span>
+              </NavLink>
+              <NavLink to="/sign-up" exact={true} activeClassName="active">
+                <img src={signup_icon} alt="signup" />
+                <span>Sign Up</span>
+              </NavLink>
+        </div>)}
+      </div>
     </nav>
   );
 }
 
 export default NavBar;
+
+{/* <ul>
+  <li className="logo_li">
+    <img id="firemoney_icon" src={icon} alt="mountain road" />
+    <NavLink class="home_link" to="/" exact={true} activeClassName="active">
+      firemoney
+          </NavLink>
+  </li>
+  <li>
+    <NavLink to="/login" exact={true} activeClassName="active">
+      Login
+          </NavLink>
+  </li>
+  <li>
+    <NavLink to="/sign-up" exact={true} activeClassName="active">
+      Sign Up
+          </NavLink>
+  </li>
+  <li>
+    <NavLink to="/users" exact={true} activeClassName="active">
+      Users
+          </NavLink>
+  </li>
+  <li>
+    <LogoutButton setAuthenticated={setAuthenticated} />
+  </li>
+</ul> */}
