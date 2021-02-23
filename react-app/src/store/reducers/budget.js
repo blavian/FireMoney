fc33d943bb800f3abec607b152589449775a144f
Ben Bolthouse <benbolthouse@gmail.com>
2021-02-23 15:11:30 -0700

    Create budget reducer create thunk

--- react-app/src/store/reducers/budget.js
+++ react-app/src/store/reducers/budget.js
@@ -1,7 +1,8 @@
 import { fetch } from "../../services/fetch";
 
 // Action constants
-const SET_BUDGET_MONTH = "session/setBudgetMonth";
+const CREATE_BUDGET_MONTH = "session/createBudgetMonth";
+const GET_BUDGET_MONTH = "session/setBudgetMonth";
 
 // State template
 const budgetMonthTemplate = {
@@ -12,13 +13,34 @@ const budgetMonthTemplate = {
 };
 
 // Action creators
-const setBudgetMonthActionCreator = (payload) => ({
-  type: SET_BUDGET_MONTH,
+const createBudgetMonthActionCreator = (payload) => ({
+  type: CREATE_BUDGET_MONTH,
+  payload,
+});
+const getBudgetMonthActionCreator = (payload) => ({
+  type: GET_BUDGET_MONTH,
   payload,
 });
 
 // Thunks
-export const setBudgetMonth = ({ monthInt, yearInt }) => async (dispatch) => {
+export const createBudgetMonth = ({
+  monthInt,
+  yearInt,
+  copyPrevious,
+}) => async (dispatch) => {
+  const res = await fetch(`/api/month`, {
+    method: "POST",
+    body: JSON.stringify({
+      month_int: monthInt,
+      year_int: yearInt,
+      copy_previous: copyPrevious,
+    }),
+  });
+  const { data } = res.data;
+  dispatch(createBudgetMonthActionCreator(data));
+  return data;
+};
+export const getBudgetMonth = ({ monthInt, yearInt }) => async (dispatch) => {
   const res = await fetch(
     `/api/month?month_int=${monthInt}&year_int=${yearInt}`,
     {
@@ -26,20 +48,32 @@ export const setBudgetMonth = ({ monthInt, yearInt }) => async (dispatch) => {
     }
   );
   const { data } = res.data;
-  dispatch(setBudgetMonthActionCreator(data));
+  dispatch(getBudgetMonthActionCreator(data));
   return data;
 };
 
 // Reducer configuration
-const reducer = (state = { budgetMonth: budgetMonthTemplate }, { type, payload }) => {
+const reducer = (
+  state = { budgetMonth: budgetMonthTemplate },
+  { type, payload }
+) => {
   switch (type) {
-    case SET_BUDGET_MONTH:
+    case CREATE_BUDGET_MONTH:
       const convertData = {
-        monthInt: payload.month_int,
-        month: getMonthFromInt(payload.month_int),
-        year: payload.year_int,
-        groups: payload.groups,
-      }
+        monthInt: payload.data.month_int,
+        month: getMonthFromInt(payload.data.month_int),
+        year: payload.data.year_int,
+        groups: payload.data.groups,
+      };
+      return { budgetMonth: { ...state.budgetMonth, ...convertData } };
+
+    case GET_BUDGET_MONTH:
+      const convertData = {
+        monthInt: payload.data.month_int,
+        month: getMonthFromInt(payload.data.month_int),
+        year: payload.data.year_int,
+        groups: payload.data.groups,
+      };
       return { budgetMonth: { ...state.budgetMonth, ...convertData } };
 
     default:
@@ -50,32 +84,32 @@ const reducer = (state = { budgetMonth: budgetMonthTemplate }, { type, payload }
 /**
  * Accepts a string integer between 1 and 12 and returns the corresponding
  * calendar month as a string.
- * 
+ *
  * @param {monthIntAsString} monthIntAsString
  * Integer as string, e.g. "12" will return "December"
- * 
+ *
  */
 export const getMonthFromInt = (monthIntAsString) => {
   for (const key in monthDict) {
     console.log(key);
     if (monthIntAsString === key) return monthDict[key];
   }
-  throw RangeError("Month integer is out of range; must be between 1 and 12.")
-}
+  throw RangeError("Month integer is out of range; must be between 1 and 12.");
+};
 
 const monthDict = {
-  "1": "January",
-  "2": "February",
-  "3": "March",
-  "4": "April",
-  "5": "May",
-  "6": "June",
-  "7": "July",
-  "8": "August",
-  "9": "September",
-  "10": "October",
-  "11": "November",
-  "12": "December",
-}
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
 
 export default reducer;
