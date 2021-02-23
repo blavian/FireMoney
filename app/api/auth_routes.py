9e257355ca9f6f96de0f7d9605e0ed8797fcbe71
Ben Bolthouse <benbolthouse@gmail.com>
2021-02-22 18:21:30 -0700

    Change API response for auth/login to be consistent with wiki specs

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -14,7 +14,7 @@ def validation_errors_to_error_messages(validation_errors):
     errorMessages = []
     for field in validation_errors:
         for error in validation_errors[field]:
-            errorMessages.append(f"{field} : {error}")
+            errorMessages.append(f'{field} : {error}')
     return errorMessages
 
 
@@ -42,7 +42,7 @@ def login():
         # Add the user to the session, we are logged in!
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
-        return user.to_auth_response()
+        return {'message': 'success', 'data': user.to_auth_response()}
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
