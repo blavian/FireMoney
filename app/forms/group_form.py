f363c5479aa33b63773887e429acab18cd631c6a
luke <lukebraun88@att.net>
2021-02-22 19:29:17 -0600

    formms

--- app/forms/group_form.py
+++ app/forms/group_form.py
@@ -4,15 +4,15 @@ from wtforms.validators import DataRequired, ValidationError
 from app.models import Budget_Group
 
 
-def group_exists(form, field):
-    print("Checking if group already exits", field.data)
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if user:
-        raise ValidationError("User is already registered.")
+# def group_exists(form, field):
+#     print("Checking if group already exits", field.data)
+#     email = field.data
+#     user = User.query.filter(User.email == email).first()
+#     if user:
+#         raise ValidationError("User is already registered.")
 
 
 class GroupForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
-    month_int = IntegerField('month', validators=[DataRequired(), group_exists])
+    month_int = IntegerField('month', validators=[DataRequired()])
     year_int = IntegerField('year', validators=[DataRequired()])
