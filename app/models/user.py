07a148f1275813bc0188dc62d5759c327b63dc96
Ben Bolthouse <benbolthouse@gmail.com>
2021-02-22 17:17:43 -0700

    Add bugfix for indentation in user model

--- app/models/user.py
+++ app/models/user.py
@@ -14,27 +14,30 @@ class User(db.Model, UserMixin):
 
     groups = db.relationship("BudgetGroup", backref="users")
 
+    @property
+    def password(self):
+        return self.hashed_password
 
-@property
-def password(self):
-    return self.hashed_password
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-@password.setter
-def password(self, password):
-    self.hashed_password = generate_password_hash(password)
-
-
-def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-
-def to_dict(self):
-    return {
-        "id": self.id,
-        "username": self.username,
-        "email": self.email,
-        "createdAt": self.createdAt,
-        "updatedAt": self.updatedAt,
-        "groups": self.groups,
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt,
+            "groups": self.groups,
+        }
+    
+    def to_auth_response(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+        }
