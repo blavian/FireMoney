8399821b084ba689ff617100b69daaeacbecd5ce
luke <lukebraun88@att.net>
2021-02-23 18:32:38 -0600

    post/get months and delete groups

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -3,14 +3,14 @@ from flask_login import login_required, current_user
 
 
 from app.models import db, BudgetGroup
-from app.forms.group_form import GroupForm,GroupUpdateForm
+from app.forms.group_form import GroupForm, GroupUpdateForm
 
 group_routes = Blueprint('budget_groups', __name__)
 
 
+# CREATE NEW BUDGET GROUP
 @group_routes.route('/', methods=['POST'])
 @login_required
-# Create (POST) a new budget group
 def new_group():
     # 1. Get user from session
     user = current_user
@@ -40,50 +40,66 @@ def new_group():
     return {"message": "success", "data": group.to_dict_on_create()}, 201
 
 
+# READ GROUPS FOR CURRENT USER
 @group_routes.route('/', methods=['GET'])
 @login_required
 def get_groups():
+    # 1. gets user from session
     user = current_user
+
+    # 2. finds groups based off of user.id
     user_groups = BudgetGroup.query.filter(
-        BudgetGroup.user_id == user.id
-    )
-    return {"user_groups": [group.to_dict() for group in user_groups]}
+        BudgetGroup.user_id == user.id)
+
+    # 3. returns users groups
+    return {"message": "success", "user_groups": [group.to_dict() for group in user_groups]}, 200
 
 
-
-
-
-#update title
+# UPDATE GROUP TITLE
 @group_routes.route('/<int:id>', methods=['PATCH'])
 @login_required
 def update_group(id):
+
+    # 1. creates form, adds csrf token
     form = GroupUpdateForm()
     form['csrf_token'].data = request.cookies['csrf_token']
+
+    # 2. find group by id and add month and year to form
     group = BudgetGroup.query.get(id)
-    form['month_int'].data = group.month_int 
-    form['year_int'].data = group.year_int 
-  
+    form['month_int'].data = group.month_int
+    form['year_int'].data = group.year_int
+
     # 3. Validate form data; if invalid return 400 bad request to user
     if not form.validate_on_submit():
         return {"message": "validation_errors", "data": form.errors}, 400
 
-    # 4. If valid then extract useful data from form
+    # 4. If valid then extract title form
     title = form.data['title']
-    group.title = title
 
+    # 5. update group title and commit changes to database
+    group.title = title
     db.session.commit()
 
-    # 7. Send 201 response to the user
+    # 6. Return message with updated group and a 201 response
     return {"message": "success", "data": group.to_dict_on_create()}, 201
 
 
-@group_routes.route('/<int:id>/delete', methods=['GET'])
+# DELETE SPECIFIED GROUP
+@group_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
-def delete_group():
+def delete_group(id):
+    # 1. Find group by id
     group = BudgetGroup.query.get(id)
-    db.session.delete(group)
-    db.session.commit()
-    return
+
+    # 2. if group exists, delete and commit, else return msg
+    if group:
+        db.session.delete(group)
+        db.session.commit()
+        return "group was successfully deleted"
+    else:
+        return "no such group exists"
+
+
 
 
 # create default groups
--- app/api/month_routes.py
+++ app/api/month_routes.py
@@ -1,56 +1,61 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
-from app.models import db, User, BudgetGroup
+from app.models import db, BudgetGroup
 month_routes = Blueprint('months', __name__)
 
 
-# http://localhost:5000/api/month?month_int=11&year_int=2021
-# get all groups based on month, year, userId
-# get group based on month and year
-@month_routes.route('/', methods=['GET'])
-@login_required
-def months():
-    month_int = request.args.get("month_int")
-    year_int = request.args.get("year_int")
-    user = current_user
-    user_groups = BudgetGroup.query.filter(
-        BudgetGroup.user_id == user.id,
-        BudgetGroup.month_int == month_int,
-        BudgetGroup.year_int == year_int
-    )
-    return {"user_groups": [group.to_dict() for group in user_groups]}
-
-
+# CREATE NEW MONTH FROM PREVIOUS
 @month_routes.route('/', methods=['POST'])
 @login_required
-# Create (POST) a new budget group
 def new_month():
-    # 1. Get user from session
-    user = current_user
-    month_int = request.json("month_int")
-    year_int = request.json("year_int")
-    print(month_int,year_int)
 
+    # 1. Get user from session and data from request
+    user = current_user
+    month_int = request.json['month_int']
+    year_int = request.json['year_int']
+
+    # 2. Get all the users groups from last month
     user_groups = BudgetGroup.query.filter(
         BudgetGroup.user_id == user.id,
         BudgetGroup.month_int == month_int,
-        BudgetGroup.year_int == year_int
-    )
+        BudgetGroup.year_int == year_int)
+
+    # 3. Conditional to determine if we need to update the year
     if month_int == 12:
         month_int = 1,
         year_int = year_int + 1
     else:
         month_int = month_int + 1
 
-
-# 5. Create the budget group
+    # 4. creates new groups for next month and adds/commits to database
+    new_groups = []
     for group in user_groups:
         new_group = BudgetGroup(
-            user_id=user.id, title=group.title, month_int=month_int, year_int= year_int)
-
-# 6. Add and commit the budget group
+            user_id=user.id, title=group.title, month_int=month_int, year_int=year_int)
+        new_groups.append(new_group)
         db.session.add(new_group)
     db.session.commit()
 
-# 7. Send 201 response to the user
-    return {"user_groups": [group.to_dict() for group in user_groups]}
+# 7. Send new month(groups) and 201 (successful creation) response
+    return {"message": "success", "user_groups": [group.to_dict() for group in new_groups]}, 201
+
+
+
+# READ CURRENT MONTH GROUPS
+@month_routes.route('/', methods=['GET'])
+@login_required
+def months():
+
+    # 1. Get month and year from url and user from session
+    month_int = request.args.get("month_int")
+    year_int = request.args.get("year_int")
+    user = current_user
+
+    # 2. Get users groups for specified month/year
+    user_groups = BudgetGroup.query.filter(
+        BudgetGroup.user_id == user.id,
+        BudgetGroup.month_int == month_int,
+        BudgetGroup.year_int == year_int)
+
+    # 3. Return user groups
+    return {"message": "success", "user_groups": [group.to_dict() for group in user_groups]}, 200
