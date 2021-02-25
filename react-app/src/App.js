9cbca9ba7b3c0e9c1cece3291241a7a83d46326e
Ben Bolthouse <benbolthouse@gmail.com>
2021-02-25 14:28:53 -0700

    Create session action to set session user; create service module for extracting url queries from URL

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import { authenticate } from "./services/auth";
 
 import Budget from "./components/Budget";
-import LandingPage from "./components/LandingPage"
+import LandingPage from "./components/LandingPage";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import User from "./components/User";
 import UsersList from "./components/UsersList";
 
-import "./index.css"
+// Redux actions imports
+import * as sessionActions from "./store/reducers/session";
+
+import "./index.css";
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
   const [loaded, setLoaded] = useState(false);
-
+  const dispatch = useDispatch();
   useEffect(() => {
-    (async() => {
+    (async () => {
       const user = await authenticate();
       if (!user.errors) {
         setAuthenticated(true);
+        dispatch(sessionActions.setSessionUser(user));
       }
       setLoaded(true);
     })();
@@ -32,18 +37,33 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar authenticated={authenticated} setAuthenticated={setAuthenticated} />
+      <NavBar
+        authenticated={authenticated}
+        setAuthenticated={setAuthenticated}
+      />
       <Switch>
         <Route path="/" exact={true}>
           <LandingPage />
         </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList/>
+        <ProtectedRoute
+          path="/users"
+          exact={true}
+          authenticated={authenticated}
+        >
+          <UsersList />
         </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
+        <ProtectedRoute
+          path="/users/:userId"
+          exact={true}
+          authenticated={authenticated}
+        >
           <User />
         </ProtectedRoute>
-        <ProtectedRoute path="/budget" exact={true} authenticated={authenticated}>
+        <ProtectedRoute
+          path="/budget"
+          exact={true}
+          authenticated={authenticated}
+        >
           <Budget />
         </ProtectedRoute>
       </Switch>
--- react-app/src/components/Budget.js
+++ react-app/src/components/Budget.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Budget() {
-  return (<h1>Budget View</h1>);
+function Budget({ monthInt, yearInt }) {
+  return (
+    <div className="budget_page_container">
+      <div className="budget_page_heading">
+        <h1 className="budget_page_heading__month_title">MONTH TITLE</h1>
+        <Link to=""></Link>
+      </div>
+    </div>
+  );
 }
 
 export default Budget;
