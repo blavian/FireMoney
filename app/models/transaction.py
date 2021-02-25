57cec33d56097a845e9ff187eea7df52dbf4a699
Ben Bolthouse <benbolthouse@gmail.com>
2021-02-25 07:15:49 -0700

    Consolidate model scopes to single to_dict and organize model associations

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -5,17 +5,30 @@ class Transaction(db.Model):
     __tablename__ = 'transactions'
 
     id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('budget_items.id'), nullable=False)
     title = db.Column(db.String(25), nullable=False)
     amount = db.Column(db.Float(3, 2), nullable=False, default=0)
-    item_id = db.Column(db.Integer, db.ForeignKey('budget_items.id'), nullable=False)
     date = db.Column(db.Date)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now())
 
+    # Associations
+    _item = db.relationship("Transaction", back_populates="_transactions")
+
+    # Association properties
+    @property
+    def item(self):
+        return _item.to_dict()
+
+    # Scope
     def to_dict(self):
         return {
+            "id": self.id,
+            "item_id": self.item_id,
             "title": self.title,
             "amount": str(self.amount),
-            "item_id": self.item_id,
             "date": self.date,
+            "parent_item": self.item,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
