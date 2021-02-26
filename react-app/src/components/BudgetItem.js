679a55c7fffebcbd23acd68b8da7b60c4ca959e6
jh9374 <39279427+jh9374@users.noreply.github.com>
2021-02-26 11:15:17 -0600

    budget item and transaction component create along with some styling

--- react-app/src/components/BudgetItem.js
+++ react-app/src/components/BudgetItem.js
@@ -1,19 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
+import Transaction from "./Transaction"
 
-function BudgetItem(){
+function BudgetItem() {
+
+    const [transactionDisplay, setTransactionDisplay] = useState(true)
+
     return (
         <div className="budget_group_items_container">
-            <div className="budget_item_title">
-                <span>Rent</span>
+            <div className="budget_group_item">
+                <div className="budget_item_title">
+                    <span>Rent</span>
+                </div>
+                <div className="budget_item_description">
+                    <span>Due at the first of the month</span>
+                </div>
+                <div className="budget_item_amount_budgeted">
+                    <span>$1040.00</span>
+                </div>
+                <div className="budget_item_amount_spent">
+                    <span>$00.00</span>
+                </div>
             </div>
-            <div className="budget_item_description">
-                <span>Due at the first of the month</span>
+            <div className="transaction_buttons">
+                <button onClick={() => setTransactionDisplay(!transactionDisplay)}>View Transactions</button>
             </div>
-            <div className="budget_item_amount_budgeted">
-                <span>$1040.00</span>
+            <div className="item_transactions_container" style={ transactionDisplay ? {display:"none"} : {display:"block"} }>
+                <div className="transaction_heading_container">
+                    <div className="transaction_heading transaction_title">
+                        <h4>Transaction Title</h4>
+                    </div>
+                    <div className="transaction_heading transaction_amount">
+                        <h4>Transaction Date</h4>
+                    </div>
+                    <div className="transaction_heading transaction_spent">
+                        <h4>Amount</h4>
+                    </div>
+                </div>
+                <Transaction />
             </div>
-            <div className="budget_item_amount_spent">
-                <span>$00.00</span>
+            <div className="add_budget_item_container">
+                <button className="add_budget_item_button">ADD ITEM</button>
             </div>
         </div>
     )
---
+++ react-app/src/components/Transaction.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Transaction() {
+    return (
+        <div className="item_transaction">
+            <div className="transaction_title">
+                <span>Transaction</span>
+            </div>
+            <div className="transaction_date">
+                <span>$1040.00</span>
+            </div>
+            <div className="transaction_amount">
+                <span>$00.00</span>
+            </div>
+        </div>
+    )
+}
+
+export default Transaction;
