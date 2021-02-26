5a2da709a605c37aa1fa9e7f430d26033c6f1b8d
Ben Bolthouse <benbolthouse@gmail.com>
2021-02-26 13:43:44 -0700

    Add logic in budget groups to get total expected amount from items

--- react-app/src/components/BudgetGroup.js
+++ react-app/src/components/BudgetGroup.js
@@ -1,29 +1,37 @@
-import React from "react"
-import BudgetItem from "./BudgetItem"
-import { useDispatch, useSelector } from "react-redux"
+import React from "react";
+import BudgetItem from "./BudgetItem";
+import { useDispatch, useSelector } from "react-redux";
 
-import * as budgetActions from "../store/reducers/budget";
+// import * as budgetActions from "../store/reducers/budget";
 
-function BudgetGroup({id}) {
-    // const budgetGroupState = useSelector(x => x.budget.budgetMonth.groups[id]);
-    const dispatch = useDispatch();
-    
-    return (
-        <div className="budget_group_container">
-            <div className="budget_group_heading_container">
-                <div className="budget_group_heading budget_group_title">
-                    <h2>Budget Group</h2>
-                </div>
-                <div className="budget_group_heading budget_group_amount">
-                    <h3>Amount Budgeted</h3>
-                </div>
-                <div className="budget_group_heading budget_group_spent">
-                    <h3>Total Spent</h3>
-                </div>
-            </div>
-            <BudgetItem />
+function BudgetGroup({ id }) {
+  const budgetGroup = useSelector((x) => x.budget.budgetMonth.groups[id]);
+  const dispatch = useDispatch();
+
+  const getItemsExpectedAmountTotal = () => {
+      let total = 0;
+      for (const key in budgetGroup.items) {
+          total += parseFloat(budgetGroup.items[key].expected_amount);
+      }
+      return total;
+  }
+
+  return (
+    <div className="budget_group_container">
+      <div className="budget_group_heading_container">
+        <div className="budget_group_heading budget_group_title">
+          <h2>{budgetGroup.title}</h2>
         </div>
-    )
+        <div className="budget_group_heading budget_group_amount">
+          <h3>{getItemsExpectedAmountTotal()}</h3>
+        </div>
+        <div className="budget_group_heading budget_group_spent">
+          <h3>Total Spent</h3>
+        </div>
+      </div>
+      <BudgetItem />
+    </div>
+  );
 }
 
 export default BudgetGroup;
