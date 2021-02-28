26e730613b6c6f98697e0b4c514eb67a2d221d0a
jh9374 <39279427+jh9374@users.noreply.github.com>
2021-02-28 14:21:03 -0600

    transaction update and delete working

--- react-app/src/components/Transaction.js
+++ react-app/src/components/Transaction.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { deleteTransaction, updateTransaction } from "../store/reducers/budget";
 
 function Transaction({ groupId, itemId, transactionId }) {
 
+  const dispatch = useDispatch();
+
+  const transactionItem = useSelector((x) => x.budget.budgetMonth.groups[groupId].items[itemId].transactions[transactionId])
 
   const [updateItemView, setUpdateItemView] = useState(false);
+  const [updatedTitle, setUpdatedTitle] = useState(transactionItem ? transactionItem.title : "");
+  const [updatedAmount, setUpdatedAmount] = useState(transactionItem ? transactionItem.amount : "");
+  const [updatedDate, setUpdatedDate] = useState(transactionItem ? transactionItem.date : "");
   // Hooks
   const transaction = useSelector((x) => {
     return groupId && itemId && transactionId
@@ -14,16 +21,25 @@ function Transaction({ groupId, itemId, transactionId }) {
         .transactions[transactionId]
       : null;
   });
-  // const dispatch = useDispatch();
 
-  function updateItem(evt) {
+  function updateTrans(evt) {
     evt.preventDefault();
+    const data = {
+      id: transactionItem.id,
+      title: updatedTitle,
+      amount: updatedAmount,
+      date: updatedDate
+    }
+    // console.log(data);
+    dispatch(updateTransaction(data));
   }
 
-  function deleteItem(evt) {
+  function deleteTrans(evt) {
     evt.preventDefault();
-    // dispatch(budgetActions.deleteBudgetItem({ id: budgetItem.id }));
-    // dispatch(budgetActions.getBudgetMonth({}))
+    const data = {
+      id: transactionItem.id
+    }
+    dispatch(deleteTransaction(data))
   }
 
   return (
@@ -31,23 +47,56 @@ function Transaction({ groupId, itemId, transactionId }) {
       {transaction ? (
         <div className="item_transaction">
           <div className="transaction_title">
-            <span>{transaction.title}</span>
+            { updateItemView ?
+              (
+                <input
+                  type="text"
+                  defaultValue={transactionItem.title}
+                  onChange={(e) => setUpdatedTitle(e.target.value)}
+                ></input>
+              ):
+              (
+                <span>{transaction.title}</span>
+              )
+            }
           </div>
           <div className="transaction_date">
-            <span>{transaction.date}</span>
+            {updateItemView ?
+              (
+                <input
+                  type="date"
+                  defaultValue={transactionItem.date}
+                  onChange={(e) => setUpdatedDate(e.target.value)}
+                ></input>
+              ) :
+              (
+                <span>{transaction.date}</span>
+              )
+            }
           </div>
           <div className="transaction_amount">
-            <span>${parseInt(transaction.amount).toFixed(2)}</span>
+            {updateItemView ?
+              (
+                <input
+                  type="text"
+                  defaultValue={transactionItem.amount}
+                  onChange={(e) => setUpdatedAmount(e.target.value)}
+                ></input>
+              ) :
+              (
+                <span>${parseInt(transaction.amount).toFixed(2)}</span>
+              )
+            }
           </div>
           <div className="budget_item_buttons">
             {!updateItemView ?
               <>
                 <button onClick={(evt) => setUpdateItemView(true)} type="button">Edit</button>
-                <button onClick={(evt) => deleteItem(evt)} type="button">Delete</button>
+                <button onClick={(evt) => deleteTrans(evt)} type="button">Delete</button>
               </>
               :
               <>
-                <button onClick={(evt) => updateItem(evt)} type="button">Update</button>
+                <button onClick={(evt) => updateTrans(evt)} type="button">Update</button>
                 <button onClick={(evt) => setUpdateItemView(false)} type="button">Cancel</button>
               </>
             }
