9fd713f2004de13eb4082cd96d4f0a180aa38c16
jh9374 <39279427+jh9374@users.noreply.github.com>
2021-04-12 23:19:37 -0500

    creating transactions reducer for all of users transactions

---
+++ react-app/src/store/reducers/transaction.js
@@ -0,0 +1,43 @@
+import { fetch } from "../../services/fetch";
+import { normalizedData } from "../../services/normalize_data";
+
+// Action constants
+const CREATE_TRANSACTION = "transaction/createTransaction";
+const GET_TRANSACTIONS = "transaction/setTransactions";
+const DELETE_TRANSACTION = "transaction/deleteTransaction";
+const UPDATE_TRANSACTION = "transaction/updateTransaction";
+
+const transactionsTemplate = {
+    transactions: {}
+}
+
+// Action creators
+const createTransactionActionCreator = (payload) => ({
+    type: CREATE_TRANSACTION,
+    payload,
+});
+
+const getTransactionsActionCreator = (payload) => ({
+    type: GET_TRANSACTIONS,
+    payload,
+});
+
+const deleteTransactionActionCreator = (payload) => ({
+    type: DELETE_TRANSACTION,
+    payload,
+});
+
+const updateTransactionActionCreator = (payload) => ({
+    type: UPDATE_TRANSACTION,
+    payload,
+});
+
+// Thunks
+
+export const getTransactions = (id) => async (dispatch) => {
+    const res = await fetch(`/api/users/${id}/transactions`, {});
+    const { data } = res.data;
+
+    dispatch(getTransactionsActionCreator(data));
+    return data;
+}
